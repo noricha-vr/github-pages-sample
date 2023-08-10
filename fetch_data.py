fda29d65f3a9d182da30e35945e619fa38e2178e
Noricha <110450172+noricha-vr@users.noreply.github.com>
2023-08-10 08:58:56 +0000

    change json url.

--- fetch_data.py
+++ fetch_data.py
@@ -4,7 +4,7 @@ import json
 
 if __name__ == '__main__':
     # 指定されたURL
-    url = 'https://script.googleusercontent.com/macros/echo?user_content_key=bBwAnCjDmXbvp1oF4TWqrn9kSmNtdj_zUlG8_BRGJch_oBo_-iAqyHBu3BEXfQGlXLEMLHhDuNX9-K4BoDPxXa0px7g-NHqkm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJdgNG2cQvZjSgoYy3SiiaVo1_sc_kd3moTIsaiIQK_BWhuoQM4-TmIwNOwUK3_lyWXu0pIj0wWr48a0p1bdH4dW0m2lyD18cNz9Jw9Md8uu&lib=MLoMIN9FtTpFMSP_bVTuP0D8rQg-QTqZL'  # *** に実際のURLを入れてください
+    url = 'https://script.google.com/macros/s/AKfycbz4SfH1mb3mMECNLrHtOmKti7dGHq1Rr5GYrbRVCEDvDLVGq_1eYC9d8qYZ-55a-BwzZQ/exec'  # *** に実際のURLを入れてください
     # URLからデータを取得する
     response = requests.get(url)
 
