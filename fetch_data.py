16e819320e24f0f69436b030ca121d39bbe21cd3
noricha-vr <noricha.vr@gmail.com>
2023-08-10 18:01:33 +0900

    change url

--- fetch_data.py
+++ fetch_data.py
@@ -4,7 +4,7 @@ import json
 
 if __name__ == '__main__':
     # 指定されたURL
-    url = 'https://script.googleusercontent.com/macros/echo?user_content_key=bBwAnCjDmXbvp1oF4TWqrn9kSmNtdj_zUlG8_BRGJch_oBo_-iAqyHBu3BEXfQGlXLEMLHhDuNX9-K4BoDPxXa0px7g-NHqkm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJdgNG2cQvZjSgoYy3SiiaVo1_sc_kd3moTIsaiIQK_BWhuoQM4-TmIwNOwUK3_lyWXu0pIj0wWr48a0p1bdH4dW0m2lyD18cNz9Jw9Md8uu&lib=MLoMIN9FtTpFMSP_bVTuP0D8rQg-QTqZL'  # *** に実際のURLを入れてください
+    url = 'https://script.googleusercontent.com/macros/echo?user_content_key=PhvMg2JKOqux5QZYM3KSzEUa-FY5nMzY9-oY0TYULcbyvvyh6gK9KDAyLx-8lMmRGlvoFHClV2bqCb0kSLK3zpkUwVGM22F-m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPxulySh2GtSKCBuTie7tvIGsvOkys3hz804skdAKb5Ah10vheb2fQbPfRPSUxAhoi7DLCMoUwEz-3nkyUpHwIKOEDEYMrI5Wtz9Jw9Md8uu&lib=MLoMIN9FtTpFMSP_bVTuP0D8rQg-QTqZL'  # *** に実際のURLを入れてください
     # URLからデータを取得する
     response = requests.get(url)
 
